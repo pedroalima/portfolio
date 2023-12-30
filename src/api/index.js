5a43a7116b7e29bf881aa8e2011070f512000694
Pedro Lima <tsc.pedrolima@gmail.com>
2023-12-29 22:09:27 -0300

    feat: add environment variables

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,19 @@
 const express = require("express")
 const cors = require("cors")
+require("dotenv").config()
 
+// App 
 const app = express()
-
 app.use(cors())
 app.use(express.json())
 
+// DB connection
 const conn = require("../database/index")
 conn()
 
-app.listen(8000, () => {
+// Server connection
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
     console.log("Server running!")
 })
