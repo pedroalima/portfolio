cf1e4b916ca78ec90034b7c145b33d39fe7d76fa
Pedro Lima <tsc.pedrolima@gmail.com>
2023-12-31 15:43:27 -0300

    feat: add crud functions in projectController file

--- src/api/controllers/projectController.js
+++ src/api/controllers/projectController.js
@@ -13,7 +13,82 @@ const projectController = {
 
             const response = await ProjectModel.create(project);
 
-            res.status(201).json({ response, msg: "Project created successfully" });
+            res
+                .status(201)
+                .json({ response, msg: "Project created successfully" });
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    getAll: async(req, res) => {
+        try {
+            const projects = await ProjectModel.find();
+
+            res.json(projects);
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    get: async(req, res) => {
+        try {
+            const id = req.params.id;
+            const project = await ProjectModel.findById(id);
+
+            if(!project) {
+                res
+                    .status(404)
+                    .json({ msg: "Project not founded"});
+            }
+
+            res.json(project);
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    delete: async(req, res) => {
+        try {
+            const id = req.params.id;
+            const project = await ProjectModel.findById(id);
+
+            if(!project) {
+                res
+                    .status(404)
+                    .json({ msg: "Project not founded"});
+            }
+
+            const deleteProject = await ProjectModel.findByIdAndDelete(id);
+
+            res
+                .status(200)
+                .json({ deleteProject, msg: "Project deleted successfully" });
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    update: async(req, res) => {
+        try {
+            const id = req.params.id;
+
+            const project = {
+                name: req.body.name,
+                website: req.body.website,
+                technologies: req.body.technologies,
+                github: req.body.github,
+            };
+
+            const updateProject = await ProjectModel.findByIdAndUpdate(id, project);
+
+            if(!updateProject) {
+                res
+                    .status(404)
+                    .json({ msg: "Project not founded"});
+            }
+
+            res.status(200).json({ project, msg: "Project update successfully"});
 
         } catch (error) {
             console.log(error);
--- src/api/routes/project.js
+++ src/api/routes/project.js
@@ -4,4 +4,12 @@ const projectController = require("../controllers/projectController");
 
 router.route("/projects").post((req, res) => projectController.create(req, res));
 
+router.route("/projects").get((req, res) => projectController.getAll(req, res));
+
+router.route("/projects/:id").get((req, res) => projectController.get(req, res));
+
+router.route("/projects/:id").delete((req, res) => projectController.delete(req, res));
+
+router.route("/projects/:id").put((req, res) => projectController.update(req, res));
+
 module.exports = router;
