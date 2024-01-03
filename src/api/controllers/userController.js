8306412a75c76fe5a9941b9d4f37896b016a9114
Pedro Lima <tsc.pedrolima@gmail.com>
2024-01-02 22:48:44 -0300

    feat: add create method in userControllers file

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -2,6 +2,23 @@
 const { User: UserModel } = require("../models/User");
 
 const userController = {
+    create: async (req, res) => {
+        try {
+            const user = {
+                username: req.body.username,
+                password: req.body.password,
+            };
+
+            const response = await UserModel.create(user);
+
+            res
+                .status(200)
+                .json({ response, msg: "User created successfully!"});
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
     get: async (req, res) => {
         try {
             const user = await UserModel.find();
