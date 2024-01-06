c9db9c388071aa5ae53f5480efca9001533002b0
Pedro Lima <tsc.pedrolima@gmail.com>
2024-01-05 23:59:12 -0300

    feat: add login page

--- src/app/(private)/login/page.tsx
+++ src/app/(private)/login/page.tsx
@@ -2,8 +2,39 @@
 
 export default function Login() {
     return (
-        <div className="h-dvh">
-            <h1>Login</h1>
+        <div className="min-h-dvh flex justify-center items-center bg-gradient-to-r from-cyan-300 to-blue-600">
+            <div className="border-b-2 pb-12 w-80 drop-shadow-lg">
+                <div className="backdrop-blur-sm bg-white/30 min-h-96 px-10 py-8 rounded-2xl flex flex-col justify-between text-zinc-500 shadow-lg">
+                    <h1 className="text-center text-4xl font-bold">Sign In</h1>
+
+                    <input 
+                        type="text" 
+                        name="username" 
+                        id="username" 
+                        placeholder="Username" 
+                        className="p-2 rounded-md text-zinc-500 font-extralight"
+                    />
+
+                    <input 
+                        type="text" 
+                        name="password" 
+                        id="password" 
+                        placeholder="Password"
+                        className="p-2 rounded-md text-zinc-500 font-extralight"
+                    />
+                    <div className="flex flex-col gap-2">
+                        <button 
+                            type="submit"
+                            className="p-2 rounded-md bg-neutral-50 font-extralight w-full"
+                        >Login</button>
+                
+                        <div className="flex gap-2">    
+                            <input type="checkbox" name="remember" id="remember" />
+                            <label htmlFor="remember" className="font-extralight text-sm">Remember me</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
