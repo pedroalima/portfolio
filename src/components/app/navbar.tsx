1ea56aa1bc9b836de8b8832e9201433cfe621435
Pedro Lima <tsc.pedrolima@gmail.com>
2024-01-06 16:06:49 -0300

    feat: add navbar component

--- src/components/app/navbar.tsx
+++ src/components/app/navbar.tsx
@@ -1,16 +1,23 @@
+import Link from "next/link";
 
+const navItems = [
+    "Home",
+    "About",
+    "Products",
+    "Experience",
+    "Education",
+    "Contact"
+];
 
 export default function Navbar() {
     return (
-        <nav className="container mx-auto p-8 flex justify-between">
-            <h1>Pedro Lima</h1>
-            <ul className="flex justify-between gap-10">
-                <li>Home</li>
-                <li>About</li>
-                <li>Projects</li>
-                <li>Experience</li>
-                <li>Education</li>
-                <li>Contact</li>
+        <nav className="container mx-auto p-8 flex justify-start gap-20">
+            <h1><strong>Pedro</strong> Lima</h1>
+
+            <ul  className="flex justify-between gap-10">
+                {navItems.map((item: string) => (
+                    <Link href={`#${item}`} key={item}>{item}</Link>
+                ))}
             </ul>
         </nav>
     );
