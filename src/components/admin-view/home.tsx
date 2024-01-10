0202050ccdaf83d5987f06326bf77b2ced5c9e4b
Pedro Lima <tsc.pedrolima@gmail.com>
2024-01-09 23:34:58 -0300

    feat: add conection zod at form

--- src/components/admin-view/home.tsx
+++ src/components/admin-view/home.tsx
@@ -1,19 +1,32 @@
-import { getData } from "@/services";
+// import { getData } from "@/services";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const schema = z.object({
+    mainText: z.string(),
+    description: z.string()
+});
+
+type FormData = z.infer<typeof schema>
 
 export default function Home() {
-    const data = getData("projects");
+    const { handleSubmit, register, formState : {errors} } = useForm<FormData>({
+        mode: "onBlur",
+        resolver: zodResolver(schema)
+    });
 
-    console.log(data);
+    console.log(errors);
 
     return (
         <div className="min-h-dvh flex justify-center items-center">
-            <form className="bg-slate-300 py-10 px-8 w-1/2 flex flex-col items-stretch gap-4">
+            <form onSubmit={handleSubmit((data) => console.log(data))} className="bg-slate-300 py-10 px-8 w-1/2 flex flex-col items-stretch gap-4">
 
-                <label htmlFor="primary">Texto Principal</label>
-                <input type="text" name="primary" id="primary" className="rounded-md p-1" />
+                <label htmlFor="mainText">Texto Principal</label>
+                <input type="text" {...register("mainText")} className="rounded-md p-1" />
 
                 <label htmlFor="description">Descrição</label>
-                <input type="text" name="description" id="description" className="rounded-md p-1" />
+                <input type="text" {...register("description")} className="rounded-md p-1" />
 
                 <button type="submit" className="bg-blue-500 self-center p-2 rounded-md text-white">Adicionar</button>
             </form>
