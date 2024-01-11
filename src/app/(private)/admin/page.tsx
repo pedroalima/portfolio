ab289799f1e27de48712d24510eb332a0d9e083e
Pedro Lima <tsc.pedrolima@gmail.com>
2024-01-10 23:54:08 -0300

    feat: add navbar from admin page

--- src/app/(private)/admin/page.tsx
+++ src/app/(private)/admin/page.tsx
@@ -1,16 +1,51 @@
 "use client";
 import About from "@/components/admin-view/about";
 import Home from "@/components/admin-view/home";
-import Navbar from "@/components/app/navbar";
+import { useState } from "react";
+
+const menuItems = [
+    {
+        id: "home",
+        label: "Home",
+        component: (
+            <Home />
+        )
+    },
+    {
+        id: "about",
+        label: "About",
+        component: (
+            <About />
+        )
+    },
+];
 
 export default function Admin() {
+    const [ currentSelectedTab, setCurrentSelectedTab ] = useState("home");
     return (
         <div className="h-dvh">
-            <Navbar />
+            <header className="fixed top-0 w-full z-30">
+                <nav className="container mx-auto p-2 flex justify-start gap-20">
+                    {menuItems.map((item) => (
+                        <button
+                            key={item.id}
+                            type="button"
+                            className="p-4 font-bold text-xl text-black"
+                            onClick={() => {
+                                setCurrentSelectedTab(item.id);
+                            }}
+                        >
+                            {item.label}
+                        </button>
+                    ))}
+                </nav>
+            </header>
             
-            <Home />
-            
-            <About />
+            <main>
+                {menuItems && menuItems.map(
+                    (item) => item.id === currentSelectedTab && item.component
+                )}
+            </main>
         </div>
     );
 }
