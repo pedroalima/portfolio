3c569a8b227935ec9438ecdb06d8a1a26415e0b6
Pedro Lima <tsc.pedrolima@gmail.com>
2024-01-12 17:34:20 -0300

    feat: add input component

--- src/components/admin-view/about.tsx
+++ src/components/admin-view/about.tsx
@@ -1,43 +1,70 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { z } from "zod";
+import Input from "../input/input";
 
-const schema = z.object({
+const formSchema = z.object({
     update: z.string(),
     numProjects: z.string(),
     timeExperience: z.string(),
     skills: z.string()
 });
 
-type FormData = z.infer<typeof schema>
+type FormData = z.infer<typeof formSchema>
+
+const inputs = [
+    {
+        htmlFor: "updates",
+        label: "Texto de atualização",
+        type: "text",
+        placeholder: "",
+        regValue: "updates"
+    },
+    {
+        htmlFor: "projects",
+        label: "Nº de projetos",
+        type: "number",
+        placeholder: "",
+        regValue: "numProjects"
+    },
+    {
+        htmlFor: "experience",
+        label: "Tempo de experiência",
+        type: "number",
+        placeholder: "",
+        regValue: "timeExperience"
+    },
+    {
+        htmlFor: "skills",
+        label: "Skills",
+        type: "text",
+        placeholder: "",
+        regValue: "skills"
+    },
+
+];
 
 export default function About() {
-    const { handleSubmit, register, formState : {errors} } = useForm<FormData>({
-        mode: "onBlur",
-        resolver: zodResolver(schema)
+    const inputsForm = useForm<FormData>({
+        resolver: zodResolver(formSchema),
     });
+
+    const { handleSubmit, formState : {errors} } = inputsForm;
 
     console.log(errors);
 
     return (
         <div className="min-h-dvh flex justify-center items-center">
-            <form onSubmit={handleSubmit(data => console.log(data))} className="bg-slate-300 py-10 px-8 w-1/2 flex flex-col items-stretch gap-4">
-                <div className="flex flex-col">
-                    <label htmlFor="update" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Texto de atualização</label>
-                    <input type="text" {...register("update")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                </div>
-
-                <label htmlFor="projects">Nº de projetos</label>
-                <input type="number" {...register("numProjects")} className="rounded-md p-1" />
-
-                <label htmlFor="experience">Quanto tempo de experiência</label>
-                <input type="number" {...register("timeExperience")} className="rounded-md p-1" />
-
-                <label htmlFor="skills">Skills</label>
-                <input type="text" {...register("skills")} className="rounded-md p-1" />
-
-                <button type="submit" className="bg-blue-500 self-center p-2 rounded-md text-white">Adicionar</button>
-            </form>
+            <FormProvider {...inputsForm}>
+                <form onSubmit={handleSubmit(data => console.log(data))} className="bg-slate-300 py-10 px-8 w-1/2 flex flex-col items-stretch gap-4">
+                    {inputs.map((input, i) => (
+                        <div key={i} className="flex flex-col">
+                            <Input htmlFor={input.htmlFor} label={input.label} type={input.type} regValue={input.regValue} />
+                        </div>
+                    ))}
+                    <button type="submit" className="bg-blue-500 self-center p-2 rounded-md text-white">Adicionar</button>
+                </form>
+            </FormProvider>
         </div>
     );
 }
