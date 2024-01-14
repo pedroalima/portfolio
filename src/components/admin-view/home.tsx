30aa30fd1bc0e5f70a17d7405bc9e50e72809721
Pedro Lima <tsc.pedrolima@gmail.com>
2024-01-14 15:39:34 -0300

    feat: add form data in home component

--- src/components/admin-view/home.tsx
+++ src/components/admin-view/home.tsx
@@ -1,35 +1,70 @@
 // import { getData } from "@/services";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
+import { useState } from "react";
 
-const homeSchema = z.object({
-    mainText: z.string(),
-    description: z.string()
-});
+type ControlsTypes = [
+    ControlsItemType,
+    ControlsItemType
+]
 
-type HomeFormData = z.infer<typeof homeSchema>
+interface ControlsItemType {
+    name: string,
+    placeholder: string,
+    type: string,
+    label: string,
+}
+
+const controls: ControlsTypes = [
+    {
+        name: "heading",
+        placeholder: "Enter heading text",
+        type: "text",
+        label: "Enter heading text",
+    },
+    {
+        name: "summary",
+        placeholder: "Enter Career summary",
+        type: "text",
+        label: "Enter Career summary",
+    },
+];
 
 export default function Home() {
-    const { handleSubmit, register, formState : {errors} } = useForm<HomeFormData>({
-        mode: "onBlur",
-        resolver: zodResolver(homeSchema)
-    });
+    const [ formData, setFormData ] = useState({});
 
-    console.log(errors);
+    const handleSaveData = () => {
+        
+    };
+
+    console.log(formData);
 
     return (
         <div className="min-h-dvh flex justify-center items-center">
-            <form onSubmit={handleSubmit((data) => console.log(data))} className="bg-slate-300 py-10 px-8 w-1/2 flex flex-col items-stretch gap-4">
+            <div className="bg-slate-300 py-10 px-8 w-1/2 flex flex-col items-stretch gap-4">
+                {controls && controls.map(controlItem => (
+                    <div key={controlItem.name}>
+                        <label htmlFor={controlItem.name}>{controlItem.label}</label>
+                        <input 
+                            type={controlItem.type}
+                            name={controlItem.name}
+                            id={controlItem.name}
+                            onChange={(e) => {
+                                setFormData(
+                                    {
+                                        ...formData,
+                                        [controlItem.name]: e.target.value,
+                                    }
+                                );
+                            }}
+                            className="rounded-md p-1"
+                        />
+                    </div>
+                ))}
 
-                <label htmlFor="mainText">Texto Principal</label>
-                <input type="text" {...register("mainText")} className="rounded-md p-1" />
-
-                <label htmlFor="description">Descrição</label>
-                <input type="text" {...register("description")} className="rounded-md p-1" />
-
-                <button type="submit" className="bg-blue-500 self-center p-2 rounded-md text-white">Adicionar</button>
-            </form>
+                <button 
+                    onClick={() => handleSaveData} 
+                    className="bg-blue-500 self-center p-2 rounded-md text-white"
+                >Adicionar</button>
+            </div>
         </div>
     );
 }
