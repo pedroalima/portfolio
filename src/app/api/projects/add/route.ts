4114b147fa233a7e30dced4de9e3309db6490482
Pedro Lima <tsc.pedrolima@gmail.com>
2024-01-24 19:35:23 -0300

    feat: add projects route

---
+++ src/app/api/projects/add/route.ts
@@ -0,0 +1,31 @@
+import connectToDatabase from "@/database";
+import Project from "@/models/Project";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function POST(req: NextRequest) {
+    try {
+        await connectToDatabase();
+        const extractData = await req.json();
+        const saveData = await Project.create(extractData);
+
+        if (saveData) {
+            return NextResponse.json({
+                success: true,
+                message: "Data saved successfully",
+            });
+        } else {
+            return NextResponse.json({
+                success: false,
+                message: "Something goes wrong! Please try again",
+            });
+        }
+    } catch (error) {
+        console.log(error);
+
+        return NextResponse.json({
+            success: false,
+            message: "Something goes wrong! Please try again",
+        });
+    }
+}
