18e52c46c091468755d2b15900ecb68a23676d0e
Pedro Lima <tsc.pedrolima@gmail.com>
2024-02-09 17:27:54 -0300

    style: adjust layout client home page

--- src/components/client-view/home/index.tsx
+++ src/components/client-view/home/index.tsx
@@ -6,12 +6,38 @@ import { ClientSectionDataTypes } from "@/types";
 import AnimationWrapper from "../animate-wrapper";
 import profile from "../../../../public/profile.png";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { useMemo } from "react";
+
+
 
 export default function ClientHome({ data } : { data: ClientSectionDataTypes}) {
+
+    function variants() {
+        return {
+            offscreen: {
+                x: 550,
+                opacity: 0,
+            },
+            onscreen: ({ duration = 1.5 } = {}) => ({
+                x: 0,
+                opacity: 1,
+                transition: {
+                    type: "spring",
+                    duration,
+                },
+            }),
+        };
+    }
+
+    const setVariants = useMemo(() => variants(), []);
+
     return (
-        <AnimationWrapper>
-            <div className="min-h-dvh p-8 flex justify-between items-center" id="home">
-                <motion.div className="w-1/2">
+        <div className="px-12" id="home">
+            <AnimationWrapper className="h-screen pt-36 flex justify-between items-center">
+                <motion.div 
+                    className="w-1/2"
+                    variants={setVariants}
+                >
                     <h1 className="mb-4 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">
                         {data.data[0].mainText
                             .split(" ")
@@ -20,7 +46,7 @@ export default function ClientHome({ data } : { data: ClientSectionDataTypes}) {
                                     key={index} 
                                     className={`${
                                         index === 1
-                                            ? "text-7xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400"
+                                            ? "text-7xl text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-cyan-400"
                                             : "text-white" 
                                     }`}
                                 >
@@ -30,19 +56,24 @@ export default function ClientHome({ data } : { data: ClientSectionDataTypes}) {
                         }
                     </h1>
                     <p className="text-lg font-normal text-gray-500 lg:text-xl">{data.data[0].description}</p>
-                    <motion.div animate={{ scale: 2}} className="flex justify-around w-20">
-                        <Link href="https://github.com/pedroalima">
-                            <FaGithub />
-                        </Link>
-                        <Link href="https://www.linkedin.com/in/pedroalima6/">
-                            <FaLinkedinIn />
-                        </Link>
-                    </motion.div>
+                    <div className="flex justify-around w-60 mt-16 ml-16">
+                        <motion.div  animate={{ scale: 1.5}} whileHover={{ scale: 2 }}>
+                            <Link href="https://github.com/pedroalima">
+                                <FaGithub color="rgba(34, 211, 238, 1)" className="w-6 h-6" />
+                            </Link>
+                        </motion.div>
+                        <motion.div  animate={{ scale: 1.5}} whileHover={{ scale: 2 }}>
+                            <Link href="https://www.linkedin.com/in/pedroalima6/">
+                                <FaLinkedinIn color="rgba(34, 211, 238, 1)" className="w-6 h-6" />
+                            </Link>
+                        </motion.div>
+                    </div>
                 </motion.div>
+
                 <motion.div className="w-1/2 flex justify-center">
                     <Image src={profile} width={500} alt="Image profile" />
                 </motion.div>
-            </div>
-        </AnimationWrapper>
+            </AnimationWrapper>
+        </div>
     );
 }
