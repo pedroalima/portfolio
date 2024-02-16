91b0b65a7f275d54d33fc3acbdc7b8f036395025
Pedro Lima <tsc.pedrolima@gmail.com>
2024-02-16 17:55:11 -0300

    style: add track and thumb scrollbar

--- src/components/client-view/projects/index.tsx
+++ src/components/client-view/projects/index.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { ClientSectionDataTypes } from "@/types";
 // import { useScroll } from "framer-motion";
-import Link from "next/link";
 import { useRef } from "react";
 import AnimationWrapper from "../animate-wrapper";
+import { useRouter } from "next/navigation";
 
 export default function ClientProjects({ data } : { data: ClientSectionDataTypes}) {
     console.log(data);
+
+    const router = useRouter();
 
     const containerRef = useRef(null);
     // const { scrollXProgress } = useScroll({ container: containerRef });
@@ -22,35 +24,18 @@ export default function ClientProjects({ data } : { data: ClientSectionDataTypes
                             <div className="border-2 w-full relative border-cyan-400 transition-all rounded-lg flex flex-col">
                                 <div className="flex flex-col xl:flex-row w-full items-stretch xl:items-center">
                                     <div className="flex flex-col m-4">
-                                        <h3 className="text-2xl">{item.name}</h3>
+                                        <h3 className="text-2xl capitalize font-bold">{item.name}</h3>
                                         <p>{item.technologies}</p>
-                                        <div className="">
-                                            <Link href={item.github} className="">GitHub</Link>
-                                            <Link href={item.website} className="">Aplicação</Link>
-                                        </div>
                                     </div>
+                                </div>
+                                <div className="absolute w-full bottom-0 justify-center flex gap-2">
+                                    <button onClick={() => router.push(item.github)} className="p-2 text-white font-semibold text-sm tracking-widest bg-cyan-400 transition-all outline-none">GitHub</button>
+                                    <button onClick={() => router.push(item.website)} className="p-2 text-white font-semibold text-sm tracking-widest bg-cyan-400 transition-all outline-none">Aplicação</button>
                                 </div>
                             </div>
                         </li>
                     ))}
                 </ul>
-                {/* <!-- Slider controls --> */}
-                <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4"/>
-                        </svg>
-                        <span className="sr-only">Previous</span>
-                    </span>
-                </button>
-                <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                    <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                        <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
-                        </svg>
-                        <span className="sr-only">Next</span>
-                    </span>
-                </button>
             </AnimationWrapper>
         </div>
     );
