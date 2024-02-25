f597524e94f2179f856350472548da957530baf9
Pedro Lima <tsc.pedrolima@gmail.com>
2024-02-25 09:48:24 -0300

    style: add carrousel in projects view page

--- src/components/client-view/projects/index.tsx
+++ src/components/client-view/projects/index.tsx
@@ -1,20 +1,42 @@
 "use client";
 import { ClientSectionDataTypes } from "@/types";
 // import { useScroll } from "framer-motion";
-import { useRef } from "react";
-import AnimationWrapper from "../animate-wrapper";
+// import { useRef } from "react";
 import { useRouter } from "next/navigation";
+// import AnimationWrapper from "../animate-wrapper";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+
 
 export default function ClientProjects({ data } : { data: ClientSectionDataTypes}) {
     const router = useRouter();
 
-    const containerRef = useRef(null);
+    // const containerRef = useRef(null);
 
     return (
         <div className="min-h-dvh flex flex-col justify-around items-center px-12" id="projects">
             <h1 className="text-5xl">Meus Projetos</h1>
+            
+            <Splide className="p-20 flex gap-5">
+                {data && data.data && data.data.map((item, i) => (
+                    <SplideSlide key={i} className="p-6 border-2 border-cyan-400 rounded-md">
+                        <div className="">
+                            <div className="">
+                                <div className="">
+                                    <h3 className="text-2xl capitalize font-bold">{item.name}</h3>
+                                    <p>{item.technologies}</p>
+                                </div>
+                            </div>
+                            <div className="">
+                                <button onClick={() => router.push(item.github)} className="p-2 text-white font-semibold text-sm tracking-widest bg-cyan-400 transition-all outline-none">GitHub</button>
+                                <button onClick={() => router.push(item.website)} className="p-2 text-white font-semibold text-sm tracking-widest bg-cyan-400 transition-all outline-none">Aplicação</button>
+                            </div>
+                        </div>
+                    </SplideSlide>
+                ))}
+            </Splide>
 
-            <AnimationWrapper>
+            {/* <AnimationWrapper>
                 <ul className="project-wrapper" ref={containerRef}>
                     {data.data.map((item, i) => (
                         <li key={i} className="w-full flex items-stretch cursor-pointer">
@@ -33,7 +55,7 @@ export default function ClientProjects({ data } : { data: ClientSectionDataTypes
                         </li>
                     ))}
                 </ul>
-            </AnimationWrapper>
+            </AnimationWrapper> */}
         </div>
     );
 }
