f3cfb09eceb1e71861df989447d716eaa22e0aa8
Pedro Lima <tsc.pedrolima@gmail.com>
2024-02-26 16:22:04 -0300

    style: add slick carrousel

--- src/components/client-view/projects/index.tsx
+++ src/components/client-view/projects/index.tsx
@@ -4,37 +4,42 @@ import { ClientSectionDataTypes } from "@/types";
 // import { useRef } from "react";
 import { useRouter } from "next/navigation";
 // import AnimationWrapper from "../animate-wrapper";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
-import "@splidejs/react-splide/css";
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function ClientProjects({ data } : { data: ClientSectionDataTypes}) {
     const router = useRouter();
 
-    // const containerRef = useRef(null);
+    const settings = {
+        accessibility:true,
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    };
 
     return (
         <div className="min-h-dvh flex flex-col justify-around items-center px-12" id="projects">
-            <h1 className="text-5xl">Meus Projetos</h1>
+            <h1 className="text-4xl">Meus Projetos</h1>
             
-            <Splide className="p-20 flex gap-5">
-                {data && data.data && data.data.map((item, i) => (
-                    <SplideSlide key={i} className="p-6 border-2 border-cyan-400 rounded-md">
-                        <div className="">
+            <div className="w-11/12 m-auto">
+                <Slider {...settings}>
+                    {data.data.map((item, i) => (
+                        <div key={i} className="p-6 h-[250px] border-2 border-cyan-400 rounded-lg">
                             <div className="">
-                                <div className="">
-                                    <h3 className="text-2xl capitalize font-bold">{item.name}</h3>
-                                    <p>{item.technologies}</p>
-                                </div>
+                                <h3 className="text-2xl capitalize font-bold">{item.name}</h3>
+                                <p>{item.technologies}</p>
                             </div>
-                            <div className="">
+                            <div className="flex justify-around">
                                 <button onClick={() => router.push(item.github)} className="p-2 text-white font-semibold text-sm tracking-widest bg-cyan-400 transition-all outline-none">GitHub</button>
                                 <button onClick={() => router.push(item.website)} className="p-2 text-white font-semibold text-sm tracking-widest bg-cyan-400 transition-all outline-none">Aplicação</button>
                             </div>
                         </div>
-                    </SplideSlide>
-                ))}
-            </Splide>
+                    ))}
+                </Slider>
+            </div>
 
             {/* <AnimationWrapper>
                 <ul className="project-wrapper" ref={containerRef}>
