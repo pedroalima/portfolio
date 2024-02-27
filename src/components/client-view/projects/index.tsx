d365ed7acd16152ed197b262c52e066600efb819
Pedro Lima <tsc.pedrolima@gmail.com>
2024-02-27 19:38:58 -0300

    style: adjust layout projects client view

--- src/components/client-view/projects/index.tsx
+++ src/components/client-view/projects/index.tsx
@@ -1,9 +1,6 @@
 "use client";
 import { ClientSectionDataTypes } from "@/types";
-// import { useScroll } from "framer-motion";
-// import { useRef } from "react";
 import { useRouter } from "next/navigation";
-// import AnimationWrapper from "../animate-wrapper";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -21,46 +18,28 @@ export default function ClientProjects({ data } : { data: ClientSectionDataTypes
     };
 
     return (
-        <div className="min-h-dvh flex flex-col justify-around items-center px-12" id="projects">
-            <h1 className="text-4xl">Meus Projetos</h1>
+        <div className="min-h-dvh flex flex-col justify-evenly items-center px-12 bg-gray-800" id="projects">
+            <div className="text-center">
+                <h1 className="text-4xl font-semibold my-12">Meus Projetos</h1>
+                <p className="font-thin text-xl">Tenha acesso a todos os meus projetos <a href="https://github.com/pedroalima?tab=repositories" className="font-thin text-cyan-400">aqui</a></p>
+            </div>
             
-            <div className="w-11/12 m-auto">
+            <div className="w-11/12 mx-auto my-10">
                 <Slider {...settings}>
                     {data.data.map((item, i) => (
-                        <div key={i} className="p-6 h-[250px] border-2 border-cyan-400 rounded-lg">
-                            <div className="">
-                                <h3 className="text-2xl capitalize font-bold">{item.name}</h3>
-                                <p>{item.technologies}</p>
+                        <div key={i} className="p-6 h-[250px] border-2 cursor-pointer relative border-cyan-400 hover:text-cyan-400 rounded-lg">
+                            <div className="h-full flex flex-col justify-start gap-10">
+                                <h3 className="text-3xl capitalize font-bold">{item.name}</h3>
+                                <p className="font-thin text-xl">{item.technologies}</p>
                             </div>
-                            <div className="flex justify-around">
-                                <button onClick={() => router.push(item.github)} className="p-2 text-white font-semibold text-sm tracking-widest bg-cyan-400 transition-all outline-none">GitHub</button>
+                            <div className="absolute bottom-0">
+                                <button onClick={() => router.push(item.github)} className="p-2 mr-6 text-white font-semibold text-sm tracking-widest bg-cyan-400 transition-all outline-none">GitHub</button>
                                 <button onClick={() => router.push(item.website)} className="p-2 text-white font-semibold text-sm tracking-widest bg-cyan-400 transition-all outline-none">Aplicação</button>
                             </div>
                         </div>
                     ))}
                 </Slider>
             </div>
-
-            {/* <AnimationWrapper>
-                <ul className="project-wrapper" ref={containerRef}>
-                    {data.data.map((item, i) => (
-                        <li key={i} className="w-full flex items-stretch cursor-pointer">
-                            <div className="border-2 w-full relative border-cyan-400 transition-all rounded-lg flex flex-col">
-                                <div className="flex flex-col xl:flex-row w-full items-stretch xl:items-center">
-                                    <div className="flex flex-col m-4">
-                                        <h3 className="text-2xl capitalize font-bold">{item.name}</h3>
-                                        <p>{item.technologies}</p>
-                                    </div>
-                                </div>
-                                <div className="absolute w-full bottom-0 justify-center flex gap-2">
-                                    <button onClick={() => router.push(item.github)} className="p-2 text-white font-semibold text-sm tracking-widest bg-cyan-400 transition-all outline-none">GitHub</button>
-                                    <button onClick={() => router.push(item.website)} className="p-2 text-white font-semibold text-sm tracking-widest bg-cyan-400 transition-all outline-none">Aplicação</button>
-                                </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </AnimationWrapper> */}
         </div>
     );
 }
