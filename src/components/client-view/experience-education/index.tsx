0c07df6c0f1a91e8fcbb9fd7d9fe57b810c6e795
Pedro Lima <tsc.pedrolima@gmail.com>
2024-03-03 10:27:54 -0300

    style: layout experience

--- src/components/client-view/experience-education/index.tsx
+++ src/components/client-view/experience-education/index.tsx
@@ -1,4 +1,5 @@
 import { ClientSectionDataTypes } from "@/types";
+import { IoArrowRedoOutline } from "react-icons/io5";
 
 export default function ClientExperience({ experience, education } : { experience: ClientSectionDataTypes, education: ClientSectionDataTypes}) {
     return (
@@ -8,23 +9,20 @@ export default function ClientExperience({ experience, education } : { experienc
             </div>
         
             <div className="flex justify-around items-start">
-                <div className="p-12 w-1/2 flex flex-col gap-20">
-                    <h3 className="text-3xl">Prática</h3>
-                    <div className="rounded-lg p-8 container-bg">
-                        <div className="flex justify-between">
-                            <span>{experience.data[0].company}</span>
-                            <div>
-                                <span>{experience.data[0].duration} / </span>
-                                <span>{experience.data[0].location}</span>
-                            </div>
+
+                <div className="p-8 w-1/2 flex flex-col gap-20">
+                    <div className="rounded-lg px-6 py-4 container-bg flex w-full items-start">
+                        <header className="text-xs mt-1 uppercase font-semibold text-gray-500 w-3/12">{experience.data[0].duration}</header>
+                        
+                        <div className="w-9/12">
+                            <h3 className="text-base">{experience.data[0].position} &#183; {experience.data[0].company}</h3>
+                            <p className="text-sm text-gray-500">{experience.data[0].location}</p>
+                            <p className="text-sm font-normal text-gray-400">{experience.data[0].jobprofile}</p>
                         </div>
-                        <h3 className="text-2xl ">{experience.data[0].position}</h3>
-                        <p className="text-lg font-normal text-gray-500">{experience.data[0].jobprofile}</p>
                     </div>
                 </div>
-                <div className="p-12 w-1/2 flex flex-col gap-20">
-                    <h3 className="text-3xl">Teórica</h3>
-                    <div className="rounded-lg p-8 container-bg">
+                <div className="p-8 w-1/2 flex flex-col gap-20">
+                    <div className="rounded-lg px-6 py-4 container-bg">
                         <div className="flex justify-between">
                             <span>{education.data[0].college}</span>
                             <span>{education.data[0].year}</span>
