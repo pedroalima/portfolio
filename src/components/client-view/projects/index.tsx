19ff1e88ba4c245443c5fcafadd32e729aeb1dc3
Pedro Lima <tsc.pedrolima@gmail.com>
2024-03-07 17:40:20 -0300

    style: adjust layout projects page

--- src/components/client-view/projects/index.tsx
+++ src/components/client-view/projects/index.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
 
 export default function ClientProjects({ data } : { data: ClientSectionDataTypes}) {
     const router = useRouter();
@@ -18,26 +19,33 @@ export default function ClientProjects({ data } : { data: ClientSectionDataTypes
     };
 
     return (
-        <div className="bg-gray-900 flex flex-col justify-evenly items-center pt-8 px-6" id="projects">
-            <div className="text-center">
-                <p className="font-thin text-xl">Tenha acesso a todos os meus projetos <a href="https://github.com/pedroalima?tab=repositories" className="font-thin text-cyan-400">aqui</a></p>
-            </div>
-            
+        <div className="bg-gray-900 flex flex-col justify-evenly items-center px-6" id="projects">
             <div className="w-11/12 mx-auto my-10">
                 <Slider {...settings}>
                     {data.data.map((item, i) => (
-                        <div key={i} className="container-bg shadow-md my-4 p-6 h-[250px] cursor-pointer relative rounded-lg">
-                            <div className="h-full flex flex-col justify-start gap-10">
-                                <h3 className="text-3xl capitalize font-bold">{item.name}</h3>
-                                <p className="font-thin text-xl">{item.technologies}</p>
-                            </div>
-                            <div className="absolute bottom-0">
-                                <button onClick={() => router.push(item.github)} className="p-2 mr-6 text-white font-semibold text-sm tracking-widest bg-cyan-400 transition-all outline-none">GitHub</button>
-                                <button onClick={() => router.push(item.website)} className="p-2 text-white font-semibold text-sm tracking-widest bg-cyan-400 transition-all outline-none">Aplicação</button>
+                        <div key={i} className="container-bg shadow-md my-4 px-6 py-4 h-[350px] flex flex-col justify-between cursor-pointer rounded-lg">
+                            <div className="h-full flex flex-col justify-between">
+                                <div className="rounded-lg self-center overflow-hidden">
+                                    <Image src="/profile-original.jpg" width={500} height={160} alt=""  />
+                                </div>
+
+                                <div className="h-1/3">
+                                    <h3 className="text-base capitalize text-white">{item.name}</h3>
+                                    <p className="text-sm text-gray-400">{item.technologies}</p>
+                                </div>
+
+                                <div className="flex justify-between">
+                                    <button onClick={() => router.push(item.github)} className="px-2 py-1 text-white font-semibold text-sm tracking-widest bg-cyan-500 hover:bg-cyan-600 transition-all outline-none rounded-xl">GitHub</button>
+                                    <button onClick={() => router.push(item.website)} className="px-2 py-1 text-white font-semibold text-sm tracking-widest bg-cyan-500 hover:bg-cyan-600 transition-all outline-none rounded-xl">Aplicação</button>
+                                </div>
                             </div>
                         </div>
                     ))}
                 </Slider>
+            </div>
+
+            <div className="text-center mt-6">
+                <p className="text-slate-500 text-sm">Tenha acesso a todos os meus projetos <a href="https://github.com/pedroalima?tab=repositories" className="text-cyan-400">aqui</a></p>
             </div>
         </div>
     );
