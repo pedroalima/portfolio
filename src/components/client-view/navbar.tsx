94227e5028720220a75998e1389fa541ead2c315
Pedro Lima <tsc.pedrolima@gmail.com>
2024-03-10 10:27:06 -0300

    style: navbar

--- src/components/client-view/navbar.tsx
+++ src/components/client-view/navbar.tsx
@@ -6,20 +6,19 @@ export default function Navbar() {
     const [ active, setActive ] = useState("about");
 
     return (
-        <header className="">
+        <header>
             <nav>
-                <div className="flex flex-col items-start gap-3">
+                <ul className="flex flex-col items-start gap-3">
                     {navItemsClient.map((item) => (
-                        <button
+                        <li
                             key={item.id}
-                            type="button"
                             onClick={() => setActive(item.id)}
-                            className={`block py-2 px-3 md:p-1 bg-transparent hover:text-cyan-400 transition-all border-b-2 border-gray-900 hover:border-b-2 hover:border-cyan-400 ${active == item.id ? "font-bold text-cyan-400 border-b-2 border-cyan-400" : "text-white"}`}
+                            className={`block py-2 px-3 md:p-1 bg-transparent hover:text-cyan-400 transition-all border-b-2 border-gray-900 hover:border-b-2 hover:border-cyan-400 ${active == item.id ? "font-bold text-cyan-400 border-b-2 border-cyan-400" : "text-gray-500"}`}
                         >
                             <a href={`#${item.id}`}>{item.label}</a>
-                        </button>
+                        </li>
                     ))}
-                </div>
+                </ul>
             </nav>
         </header>
     );
