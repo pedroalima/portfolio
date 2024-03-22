5c47646c04f348471cd1889a345ea477ad3e8d88
Pedro Lima <tsc.pedrolima@gmail.com>
2024-03-22 17:41:57 -0300

    style: add hover elements to projects items

--- src/components/client-view/projects/index.tsx
+++ src/components/client-view/projects/index.tsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/navigation";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
+import Link from "next/link";
+import { RiArrowRightUpLine } from "react-icons/ri";
 
 export default function ClientProjects({ data } : { data: FormDataTypes[]}) {
     const router = useRouter();
@@ -13,19 +15,20 @@ export default function ClientProjects({ data } : { data: FormDataTypes[]}) {
             <ul className="flex flex-col gap-5">
                 {data.map((item, i) => (
                     <li key={i} className="container-bg px-6 py-4 rounded-lg shadow-md flex items-start gap-4">
-                        <div className="relative w-1/4 h-20">
-                            <Image src={`/${item.name.split(" ").join("")}.png`} priority fill={true} className="absolute object-cover rounded-md border-2 border-slate-200/10 transition" alt="" />
+                        <div className="relative w-1/4 h-20 cursor-pointer" onClick={() => router.push(item.website)}>
+                            <Image src={`/${item.name.split(" ").join("")}.png`} priority fill={true} className="absolute object-cover rounded-md border-2 border-slate-200/10 transition hover:border-slate-100/30" alt="" />
                         </div>
                 
-                        <div className="w-2/4">
+                        <div className="w-2/4 flex flex-col">
                             <div className="">
-                                <h3 className="text-base capitalize text-white">{item.name}</h3>
+                                <Link target="_blank" href={item.website} className="text-base font-semibold capitalize text-white hover:text-cyan-500 group">
+                                    <span className="relative">{item.name} <RiArrowRightUpLine className="absolute transition-all top-1 -right-4 group-hover:top-0 group-hover:-right-5" /></span> 
+                                </Link>
                                 <p className="text-sm text-gray-400">{item.technologies}</p>
                             </div>
 
-                            <div className="flex">
-                                <button onClick={() => router.push(item.github)} className="px-2 py-1 text-white font-semibold text-sm tracking-widest container-bg hover:bg-cyan-500 rounded-lg">GitHub</button>
-                                <button onClick={() => router.push(item.website)} className="px-2 py-1 text-white font-semibold text-sm tracking-widest container-bg hover:bg-cyan-500 rounded-lg">Aplicação</button>
+                            <div className="flex self-end">
+                                <Link href={item.github} className="px-2 py-1 text-cyan-500 font-semibold text-sm">GitHub</Link>
                             </div>
                         </div>
                     </li>
