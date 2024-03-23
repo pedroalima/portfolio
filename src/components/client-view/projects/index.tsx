cc5c48959e20a1f078344ea0b0a301d03116b8cc
Pedro Lima <tsc.pedrolima@gmail.com>
2024-03-23 12:07:23 -0300

    style: adjustments to the design of project page items

--- src/components/client-view/projects/index.tsx
+++ src/components/client-view/projects/index.tsx
@@ -6,12 +6,13 @@ import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
 import Link from "next/link";
 import { RiArrowRightUpLine } from "react-icons/ri";
+import { IoIosArrowRoundForward } from "react-icons/io";
 
 export default function ClientProjects({ data } : { data: FormDataTypes[]}) {
     const router = useRouter();
 
     return (
-        <section className="flex flex-col justify-evenly items-center pb-24" id="projects">
+        <section className="flex flex-col justify-evenly items-center pt-8 pb-24" id="projects">
             <ul className="flex flex-col gap-5">
                 {data.map((item, i) => (
                     <li key={i} className="container-bg px-6 py-4 rounded-lg shadow-md flex items-start gap-4">
@@ -19,16 +20,19 @@ export default function ClientProjects({ data } : { data: FormDataTypes[]}) {
                             <Image src={`/${item.name.split(" ").join("")}.png`} priority fill={true} className="absolute object-cover rounded-md border-2 border-slate-200/10 transition hover:border-slate-100/30" alt="" />
                         </div>
                 
-                        <div className="w-2/4 flex flex-col">
+                        <div className="w-3/4 flex flex-col">
                             <div className="">
-                                <Link target="_blank" href={item.website} className="text-base font-semibold capitalize text-white hover:text-cyan-500 group">
-                                    <span className="relative">{item.name} <RiArrowRightUpLine className="absolute transition-all top-1 -right-4 group-hover:top-0 group-hover:-right-5" /></span> 
+                                <Link 
+                                    target="_blank" 
+                                    href={item.website} 
+                                    className="text-base font-semibold capitalize text-white hover:text-cyan-500 group">
+                                    <span className="relative">{item.name} <RiArrowRightUpLine size={20} className="absolute transition-all top-0 -right-5 group-hover:-top-[2px] group-hover:-right-[22px]" /></span> 
                                 </Link>
-                                <p className="text-sm text-gray-400">{item.technologies}</p>
+                                <p className="text-sm text-gray-400 mt-3">{item.technologies}</p>
                             </div>
 
-                            <div className="flex self-end">
-                                <Link href={item.github} className="px-2 py-1 text-cyan-500 font-semibold text-sm">GitHub</Link>
+                            <div className="flex self-end group">
+                                <Link href={item.github} className="px-4 py-1 text-white hover:text-cyan-500 font-semibold text-sm relative">GitHub <IoIosArrowRoundForward size={25} className="absolute transition-all -right-2 group-hover:-right-3 top-[2px]" /></Link>
                             </div>
                         </div>
                     </li>
